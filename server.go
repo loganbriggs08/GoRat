797281842514ce572d8e5da7db6de8ece4a48aa7
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-22 18:16:27 +0100

    Run function to create database tables

--- server.go
+++ server.go
@@ -3,15 +3,21 @@ package main
 import (
 	"net/http"
 
+	"github.com/NotKatsu/GoRat/database"
+
 	"github.com/NotKatsu/GoRat/endpoints"
 	"github.com/pterm/pterm"
 )
 
 func main() {
-	http.HandleFunc("/connection/new", endpoints.ConnectionNew)
-	err := http.ListenAndServe(":8080", nil)
+	if database.CreateTables() == true {
+		http.HandleFunc("/connection/new", endpoints.ConnectionNew)
+		err := http.ListenAndServe(":8080", nil)
 
-	if err != nil {
-		pterm.Fatal.WithFatal(true).Println(err)
+		if err != nil {
+			pterm.Fatal.WithFatal(true).Println(err)
+		}
+	} else {
+		pterm.Fatal.WithFatal(true).Println("There was an error when the database tables were trying to be created.")
 	}
 }
