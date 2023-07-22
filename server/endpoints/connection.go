6a8b90368c972c139505009b03eb8df2d5f63bde
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-22 17:20:03 +0100

    Check to make sure correct method was used else return error

--- server/endpoints/connection.go
+++ server/endpoints/connection.go
@@ -1,10 +1,39 @@
 package endpoints
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/pterm/pterm"
 )
 
+type MethodError struct {
+	ErrorCode    uint64 `json:"error_code"`
+	ErrorMessage string `json:"error_message"`
+}
+
 func ConnectionNew(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
+	if r.Method == "POST" {
+		fmt.Println("Correct Method")
+	} else {
+		NewMethodError := MethodError{
+			ErrorCode:    http.StatusMethodNotAllowed,
+			ErrorMessage: "POST is the only accepted Method for this endpoint.",
+		}
+
+		w.WriteHeader(http.StatusMethodNotAllowed)
+
+		NewReturnMethodError, err := json.Marshal(NewMethodError)
+
+		if err != nil {
+			pterm.Fatal.WithFatal(true).Println(err)
+		} else {
+			_, err := w.Write(NewReturnMethodError)
+
+			if err != nil {
+				pterm.Fatal.WithFatal(true).Println(err)
+			}
+		}
+	}
 }
