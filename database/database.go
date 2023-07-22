962c1ecb61db52f74276aa592fe6288ce3c5c144
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-22 18:08:29 +0100

    Update database.go

--- database/database.go
+++ database/database.go
@@ -1,5 +1,17 @@
 package database
 
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
 func CreateTables() bool {
-	
+	database, err := sql.Open("sqlite3", "database.db")
+
+	if err != nil {
+		return false
+	} else {
+		database.Exec("CREATE TABLE IF NOT EXISTS connections()")
+	}
 }
