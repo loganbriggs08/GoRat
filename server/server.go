38ff9952270bef549806b2c8201a410371765009
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-22 17:06:34 +0100

    Listen and serve

--- server/server.go
+++ server/server.go
@@ -4,9 +4,14 @@ import (
 	"net/http"
 
 	"github.com/NotKatsu/Otter/endpoints"
+	"github.com/pterm/pterm"
 )
 
 func main() {
-	http.HandleFunc("/connection/new", endpoints.ConnectionNew())
-	
+	http.HandleFunc("/connection/new", endpoints.ConnectionNew)
+	err := http.ListenAndServe(":6969", nil)
+
+	if err != nil {
+		pterm.Fatal.WithFatal(true).Println(err)
+	}
 }
