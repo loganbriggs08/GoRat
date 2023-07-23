c99811a57499173d4d60bd05800870d64624d759
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-23 16:36:15 +0100

    Create connectedMachines function to get connected machines count

--- modules/gui/windows/windows.go
+++ modules/gui/windows/windows.go
@@ -2,6 +2,7 @@ package windows
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/NotKatsu/GoRat/modules/database"
 
@@ -10,13 +11,32 @@ import (
 	go_rat "github.com/AllenDang/giu"
 )
 
+func connectedMachines() int64 {
+	var connectionCount int64
+	connections := database.GetConnections()
+
+	for _, conn := range connections {
+		storedTime, err := time.Parse("2006-01-02 15:04:05.999999999-07:00", conn.LastHeartbeatTime)
+
+		if err != nil {
+			fmt.Println("Error parsing time:", err)
+		}
+		currentTime := time.Now()
+		timeDifference := currentTime.Sub(storedTime)
+
+		if timeDifference < 5 {
+			connectionCount += 1
+		}
+	}
+	return connectionCount
+}
 func MainWindow() {
 	go_rat.SingleWindow().Layout(
 
 		go_rat.Label(fmt.Sprint("Logs:")),
-		go_rat.ListBox("Logs", []string{"Listening on port 8080...", "New Connection from AABE9B69-76A3-4344-A2CC-9540838DBJAA", "New Connection from AABE9B69-76A3-4344-A2CC-9540838DBJAA"}).Size(go_rat.Auto, 200),
+		go_rat.ListBox("Logs", components.CreateClientArray()).Size(go_rat.Auto, 200),
 
-		go_rat.Label(fmt.Sprintf("Connected Machines (%d)", len(database.GetConnections()))),
+		go_rat.Label(fmt.Sprintf("Connected Machines (%d)", connectedMachines())),
 
 		go_rat.Table().Columns(
 			go_rat.TableColumn("MAC Address"),
