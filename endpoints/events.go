fbaad9b9593e308e9cedd649500d76e28bd197eb
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-23 11:23:48 +0100

    Check to ensure that the correct method was used.

--- endpoints/events.go
+++ endpoints/events.go
@@ -1,10 +1,33 @@
 package endpoints
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/pterm/pterm"
 )
 
 func EventNew(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Endpoint reached.")
+	if r.Method == "POST" {
+		fmt.Fprint(w, "Correct method was used.")
+	} else {
+		NewError := Error{
+			ErrorCode:    http.StatusMethodNotAllowed,
+			ErrorMessage: "POST is the only accepted Method for this endpoint.",
+		}
+		w.WriteHeader(http.StatusMethodNotAllowed)
+
+		NewReturnError, err := json.Marshal(NewError)
+
+		if err != nil {
+			pterm.Fatal.WithFatal(true).Println(err)
+		} else {
+			_, err := w.Write(NewReturnError)
+
+			if err != nil {
+				pterm.Fatal.WithFatal(true).Println(err)
+			}
+		}
+	}
 }
