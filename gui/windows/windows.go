8070a24afe45f09b13309b680c7c4802ee0fef9b
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-23 13:45:46 +0100

    Create windows.go

---
+++ gui/windows/windows.go
@@ -0,0 +1,22 @@
+package windows
+
+import (
+	"fmt"
+	
+	"github.com/NotKatsu/GoRat/gui/components"
+	
+	go_rat "github.com/AllenDang/giu"
+)
+
+func MainWindow() {
+	go_rat.SingleWindow().Layout(
+		go_rat.Label(fmt.Sprint("Logs:")),
+		go_rat.ListBox("Logs", []string{"Listening on port 8080...", "New Connection from AABE9B69-76A3-4344-A2CC-9540838DBJAA", "New Connection from AABE9B69-76A3-4344-A2CC-9540838DBJAA"}).Size(950, 200),
+
+		go_rat.Label(fmt.Sprintf("Connected Machines (%d)", 4)),
+
+		go_rat.Table().Columns(
+			go_rat.TableColumn("MAC Address"),
+			go_rat.TableColumn("OS"),
+			go_rat.TableColumn("Name")).Rows(components.CreateClientTable()...))
+}
