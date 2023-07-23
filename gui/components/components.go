779bbe707d6237578ca87d30ba70d7ab4ddc27b9
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-23 13:45:51 +0100

    Create components.go

---
+++ gui/components/components.go
@@ -0,0 +1,22 @@
+package components
+
+import (
+	
+	go_rat "github.com/AllenDang/giu"
+)
+
+func CreateClientTable() []*go_rat.TableRowWidget {
+	rows := make([]*go_rat.TableRowWidget, 0)
+
+	for i := 1; i < 5; i++ {
+		row := go_rat.TableRow(
+			go_rat.Label("AABE9B69-76A3-4344-A2CC-9540838DBJAA"),
+			go_rat.Label("Windows 11 Home"),
+			go_rat.Label("Joe Bloggs"),
+			)
+
+		rows = append(rows, row)
+	}
+
+	return rows
+}
