f3d94bf1e8596ff9c9febe2bad97e47aad49dc84
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-23 13:46:22 +0100

    Remove old none needed components from server.go

--- server.go
+++ server.go
@@ -3,43 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
-
+	
 	"github.com/NotKatsu/GoRat/modules/database"
-
 	"github.com/NotKatsu/GoRat/modules/endpoints"
 	"github.com/pterm/pterm"
 
 	go_rat "github.com/AllenDang/giu"
 )
-
-func CreateClientTable() []*go_rat.TableRowWidget {
-	rows := make([]*go_rat.TableRowWidget, 0)
-
-	for i := 1; i < 5; i++ {
-		row := go_rat.TableRow(
-			go_rat.Label("AABE9B69-76A3-4344-A2CC-9540838DBJAA"),
-			go_rat.Label("Windows 11 Home"),
-			go_rat.Label("Joe Bloggs"),
-		)
-
-		rows = append(rows, row)
-	}
-
-	return rows
-}
-
-func mainWindow() {
-	go_rat.SingleWindow().Layout(
-		go_rat.Label(fmt.Sprint("Logs:")),
-		go_rat.ListBox("Logs", []string{"Listening on port 8080...", "New Connection from AABE9B69-76A3-4344-A2CC-9540838DBJAA", "New Connection from AABE9B69-76A3-4344-A2CC-9540838DBJAA"}).Size(950, 200),
-
-		go_rat.Label(fmt.Sprintf("Connected Machines (%d)", 4)),
-
-		go_rat.Table().Columns(
-			go_rat.TableColumn("MAC Address"),
-			go_rat.TableColumn("OS"),
-			go_rat.TableColumn("Name")).Rows(CreateClientTable()...))
-}
 
 func main() {
 	if database.CreateTables() {
@@ -48,6 +18,7 @@ func main() {
 			http.HandleFunc("/connection/heartbeat", endpoints.ConnectionHeartbeat)
 
 			err := http.ListenAndServe(":8080", nil)
+
 			if err != nil {
 				pterm.Fatal.WithFatal(true).Println(err)
 			}
