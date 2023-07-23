6548d674d59d02eadd02320cdabfba393af1c113
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-23 11:33:41 +0100

    Delete old error message for unauthorized

--- endpoints/events.go
+++ endpoints/events.go
@@ -11,39 +11,27 @@ func EventNew(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		if r.Header.Get("Authorization") == "" {
 			NewError := Error{
-				ErrorCode:    http.StatusUnauthorized,
+				ErrorCode: http.StatusUnauthorized,
 				ErrorMessage: "Content is missing from request headers.",
 			}
-			w.WriteHeader(http.StatusUnauthorized)
+			
+		}
+	} else {
+		NewError := Error{
+			ErrorCode:    http.StatusMethodNotAllowed,
+			ErrorMessage: "POST is the only accepted Method for this endpoint.",
+		}
+		w.WriteHeader(http.StatusMethodNotAllowed)
 
-			NewReturnError, err := json.Marshal(NewError)
+		NewReturnError, err := json.Marshal(NewError)
 
-			if err != nil {
-				pterm.Fatal.WithFatal(true).Println(err)
-			} else {
-				_, err := w.Write(NewReturnError)
-
-				if err != nil {
-					pterm.Fatal.WithFatal(true).Println(err)
-				}
-			}
+		if err != nil {
+			pterm.Fatal.WithFatal(true).Println(err)
 		} else {
-			NewError := Error{
-				ErrorCode:    http.StatusMethodNotAllowed,
-				ErrorMessage: "POST is the only accepted Method for this endpoint.",
-			}
-			w.WriteHeader(http.StatusMethodNotAllowed)
-
-			NewReturnError, err := json.Marshal(NewError)
+			_, err := w.Write(NewReturnError)
 
 			if err != nil {
 				pterm.Fatal.WithFatal(true).Println(err)
-			} else {
-				_, err := w.Write(NewReturnError)
-
-				if err != nil {
-					pterm.Fatal.WithFatal(true).Println(err)
-				}
 			}
 		}
 	}
