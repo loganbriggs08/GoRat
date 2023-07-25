bc8512f9aac40f3aa4415a4891d2cc58bab6f2b9
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-25 12:23:17 +0100

    Check to make sure the correct method was used.

--- modules/endpoints/events.go
+++ modules/endpoints/events.go
@@ -1,10 +1,34 @@
 package endpoints
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/pterm/pterm"
 )
 
 func EventsGet(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Hello World")
+	if r.Method == "GET" {
+		fmt.Fprint(w, "Correct method was used.")
+		
+	} else {
+		wrongMethodError := Error{
+			ErrorCode:    http.StatusMethodNotAllowed,
+			ErrorMessage: "GET is the only accepted Method for this endpoint.",
+		}
+		w.WriteHeader(http.StatusMethodNotAllowed)
+
+		wrongMethodErrorJson, err := json.Marshal(wrongMethodError)
+
+		if err != nil {
+			pterm.Fatal.WithFatal(true).Println(err)
+		} else {
+			_, err := w.Write(wrongMethodErrorJson)
+
+			if err != nil {
+				pterm.Fatal.WithFatal(true).Println(err)
+			}
+		}
+	}
 }
